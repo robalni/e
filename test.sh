fb1a09e75eaf04d9ce7c854cd04387ca4a87c45f
Robert Alm Nilsson <rorialni@gmail.com>
2019-06-26 08:08:46 +0200

    Define LINUX in test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 # Usage: ./test.sh
 
 [ -z "$CXX" ] && CXX=c++
-CXXFLAGS="-Wall -Wextra -g -Isrc -DENTRY=test_main -DTEST $CXXFLAGS"
+CXXFLAGS="-Wall -Wextra -g -Isrc -DENTRY=test_main -DTEST -DLINUX $CXXFLAGS"
 LDFLAGS=""
 
 cmd="$CXX $CXXFLAGS all.cpp $LDFLAGS -o test"
