71ed938f110f867f32d44e4bb7e3321b9745014b
Robert Alm Nilsson <rorialni@gmail.com>
2019-07-03 21:49:58 +0200

    lists

--- src/editor.cpp
+++ src/editor.cpp
@@ -8,21 +8,28 @@ editor_main(int, char**) {
         return 1;
     }
 
-    Buffer buf = Buffer::new_empty(MemoryManager::new_default());
-    buf.insert_char('H', 0);
-    buf.insert_char('i', 1);
-    buf.insert_char('!', 2);
-    buf.insert_char('\n', 3);
-    View bv = View::new_into_buffer(&buf);
+    List<Buffer> buffers = List<Buffer>::new_empty();
 
-    Buffer::TmpCursor& cur = bv.cursor;
+    Buffer buf = Buffer::new_empty(MemoryManager::new_default());
+    List<Buffer>::Node list_node {buf, null, null};
+    buffers.add_last(&list_node);
+
+#define MAX_VIEWS 64
+    View views[MAX_VIEWS];
+    size_t n_views = 0;
+
+    views[0] = View::new_into_buffer(&buf);
+
+    View& active_view = views[0];
+
+    Buffer::TmpCursor& cur = active_view.cursor;
 
     // Main loop
     for (;;) {
         Event ev = read_input();
         if (ev.type & Event::CHAR) {
             buf.insert_char_at_cursor(ev.keysym, cur);
-            render_everything(bv);  // TODO: Don't need to.
+            render_everything(active_view);  // TODO: Don't need to.
         }
         if (ev.type & Event::KEYDOWN) {
             switch (ev.keysym) {
@@ -33,13 +40,13 @@ editor_main(int, char**) {
                 cur.prev_char();
                 break;
             }
-            render_everything(bv);  // TODO: Don't need to.
+            render_everything(active_view);  // TODO: Don't need to.
         }
         if (ev.type & Event::QUIT) {
             break;
         }
         if (ev.type & Event::RENDER) {
-            render_everything(bv);
+            render_everything(active_view);
         }
     }
 
